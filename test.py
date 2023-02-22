63a84efe3695455b4b585ca93bbd79a9ce550899
kizzy8848 <2823037214@qq.com>
2023-02-22 01:38:06 -0500

    Final

--- test.py
+++ test.py
@@ -1,31 +1,61 @@
 import unittest
-from toyWaterPitcherProblem import Solver
+from toyWaterPitcherProblem import Solver, read_file
 
 class TestCalculator(unittest.TestCase):
     def test01(self):
-        solver = Solver()
-        result = solver.A_star("input.txt")
+        
+        print("Test01")
+        capacities, target = read_file("input.txt")
+        solver = Solver(capacities,target)
+        result = solver.A_star()
         self.assertEqual(result,19)
+        print('****************')
+        
+        
 
     def test02(self):
-        solver = Solver()
-        result = solver.A_star("input1.txt")
+        print()
+        print()
+        print("Test02")
+        capacities, target = read_file("input1.txt")
+        solver = Solver(capacities,target)
+        result = solver.A_star()
         self.assertEqual(result,7)
+        print('****************')
+        
 
     def test03(self):
-        solver = Solver()
-        result = solver.A_star("input2.txt")
+        print()
+        print()
+        print("Test03")
+        capacities, target = read_file("input2.txt")
+        solver = Solver(capacities,target)
+        result = solver.A_star()
         self.assertEqual(result,-1)
+        print('****************')
+        
 
     def test04(self):
-        solver = Solver()
-        result = solver.A_star("input3.txt")
+        print()
+        print()
+        print("Test04")
+        capacities, target = read_file("input3.txt")
+        solver = Solver(capacities,target)
+        result = solver.A_star()
         self.assertEqual(result,-1)
+        print('****************')
+        
 
     def test05(self):
-        solver = Solver()
-        result = solver.A_star("input4.txt")
+        print()
+        print()
+        print("Test05")
+        capacities, target = read_file("input4.txt")
+        solver = Solver(capacities,target)
+        result = solver.A_star()
         self.assertEqual(result,36)
+        print('****************')
+        
 
 
 if __name__ == '__main__':
--- toyWaterPitcherProblem.py
+++ toyWaterPitcherProblem.py
@@ -1,10 +1,28 @@
 import heapq
 import math
 
+def read_file(filename):
+    # read data from file
+    fo = open(filename, mode='r')
+    line = fo.readline()
+    if line[-1] == '\n':
+        line = line[:-1]
+    capacities = [int(c) for c in line.split(',')]
+
+    line = fo.readline()
+    if line[-1] == '\n':
+        line = line[:-1]
+    target = int(line)
+    fo.close()
+    return capacities, target
 
 class Solver:
     capacities = []  # the capacity of each jug
     target = 0  # the target
+
+    def __init__(self, capacities, target):
+        self.capacities = capacities
+        self.target = target
 
     # calculate h(n)
     def heuristicFunc(self, state, max_capacity):
@@ -13,22 +31,8 @@ class Solver:
         h = math.ceil(remain / max_capacity)
         return h
 
-    def A_star(self, filename):
-        # read data in file
-        fo = open(filename, mode='r')
-
-        line = fo.readline()
-        if line[-1] == '\n':
-            line = line[:-1]
-        self.capacities = [int(c) for c in line.split(',')]
-
-        line = fo.readline()
-        if line[-1] == '\n':
-            line = line[:-1]
-        self.target = int(line)
-        print(self.capacities)
-        print(self.target)
-        fo.close()
+    def A_star(self):
+        
 
         # if gcd is not divisible by target, return -1
         # greatest common divisor 最大公约数
@@ -38,40 +42,31 @@ class Solver:
         for i in self.capacities:
             greates_common_divisor = math.gcd(greates_common_divisor, i)
         if self.target % greates_common_divisor != 0:
-            print('Gcd is not divisible by target')
-            print('Returned -1')
+            print('Fail')
+            print('Result -1')
             return -1
 
-        initial_state = 0  # interger Initial State
+        initial_state = 0  # interger Initial State 
 
-        visited = set()
-
+        visited = set() 
         visited.add(initial_state)
-
-        waters = [0 for _ in range(len(self.capacities))]
-
+        waters = [0 for _ in range(len(self.capacities))] #把所有罐子里的水初始化为0
         heap = [(self.heuristicFunc(initial_state, max(self.capacities)), 0, self.heuristicFunc(
             initial_state, max(self.capacities)), waters, initial_state)]
-
         while heap:
-
             # curr_water 是每个罐子里现有的水
             # curr_state 是无限水壶中现有的水
-
             f, g, h, curr_water, curr_state = heapq.heappop(heap)
-
             # deque the answer, return
             if curr_state == self.target:
-                print("Search Successed")
-                print('Steps Taken', g)
+                print("Success")
+                print('Result', g)
                 return g
-
             # pour any water pitcher -> the infinite one
             for i in range(len(self.capacities)):
                 # if the new state is visited, just skip
                 if curr_state + self.capacities[i] in visited:
                     continue
-
                 new_water = curr_water[:]
                 if new_water[i]:  # 如果水壶有水 清空
                     new_water[i] = 0
@@ -79,15 +74,12 @@ class Solver:
                 else:
                     new_g = g + 2  # 水壶装水 然后装到无限水壶
                 # calculate the new state related variables
-
                 new_state = curr_state + self.capacities[i]
-
                 new_h = self.heuristicFunc(new_state, max(self.capacities))
                 new_f = new_g + new_h
                 visited.add(new_state)
                 heapq.heappush(heap, (new_f, new_g, new_h,
                                       new_water, new_state))
-
             # pour the infinite one -> any water pitcher
             for i in range(len(self.capacities)):
                 # if the new state is invalid or visited, just skip
@@ -97,9 +89,7 @@ class Solver:
                 # 已访问的状态
                 if curr_state - self.capacities[i] in visited:
                     continue
-
                 new_water = curr_water[:]
-
                 # 罐子里有水先清空 再把无限水壶里的水倒进去
                 if new_water[i]:
                     new_g = g + 2
@@ -115,13 +105,12 @@ class Solver:
                 visited.add(new_state)
                 heapq.heappush(heap, (new_f, new_g, new_h,
                                       new_water, new_state))
-
-        print('Search Failed')
-        print("Returned -1")
+        print('Fail')
+        print("Result: -1")
         return -1
 
-
 if __name__ == '__main__':
-    solver = Solver()
-    steps = solver.A_star('input5.txt')
+    capacities, target = read_file("input.txt")
+    solver = Solver(capacities,target)
+    steps = solver.A_star()
     print(steps)
